98d57e744d1d8d50acb042a250b1613029cea973
Karl Broman <kbroman@gmail.com>
2018-12-04 11:50:09 +0530

    Fix bitly link in mpp live coding script
     - mpp2018 -> rqtl2_2018

--- 4_mpp/mpp_2018-12-06.R
+++ 4_mpp/mpp_2018-12-06.R
@@ -2,4 +2,4 @@
 # Live coding at ICRISAT workshop 2018-12-06
 #
 # largely following https://kbroman.org/qtl2/assets/vignettes/user_guide.html
-# latest version of this script at https://bit.ly/mpp2018
+# latest version of this script at https://bit.ly/rqtl2_2018
