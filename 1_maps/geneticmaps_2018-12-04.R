eb794500c4d9c909f2a43aa3395ff75c76d6bd95
Karl Broman <kbroman@gmail.com>
2018-12-04 10:34:09 +0530

    Start R script for live coding on genetic maps

---
+++ 1_maps/geneticmaps_2018-12-04.R
@@ -0,0 +1,5 @@
+# Genetic Map Construction
+# Live coding at ICRISAT workshop 2018-12-04
+#
+# largely following http://rqtl.org/tutorials/geneticmaps.pdf
+# latest version of this script at https://bit.ly/genmaps2018
