e44b504a9e33b8839eb390f0270458deda0eea6c
Karl Broman <kbroman@gmail.com>
2018-12-05 09:10:11 +0530

    update introqtl_2018-12-05.R 2018-12-05 09:10:11

--- 2_introqtl/introqtl_2018-12-05.R
+++ 2_introqtl/introqtl_2018-12-05.R
@@ -3,3 +3,11 @@
 #
 # largely following http://rqtl.org/tutorials/rqtltour2.pdf
 # latest version of this script at https://bit.ly/introqtl2018
+
+# load R/qtl
+library(qtl)
+
+# import some QTL mapping data
+sug <- read.cross("csv", "http://rqtl.org", "sug.csv",
+                  genotypes=c("CC", "CB", "BB"), 
+                  alleles=c("C", "B"))
