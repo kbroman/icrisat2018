10f4dfa829a654c46d3cee9fe2e69dd3a652ef24
Karl Broman <kbroman@gmail.com>
2018-12-05 09:29:54 +0530

    update introqtl_2018-12-05.R 2018-12-05 09:29:54

--- 2_introqtl/introqtl_2018-12-05.R
+++ 2_introqtl/introqtl_2018-12-05.R
@@ -28,3 +28,5 @@ plotPheno(sug, pheno.col="bw")
 
 # first step in QTL analysis: calculate genotype probabilities
 sug <- calc.genoprob(sug, step=1)
+
+# 2nd step in QTL analysis: do the LOD score calculations by interval mapping
