0dd89c7bf7c76554d94c74bafceebdcf1f92feed
Karl Broman <kbroman@gmail.com>
2018-12-06 10:37:01 +0530

    update mpp_2018-12-06.R 2018-12-06 10:37:01

--- 4_mpp/mpp_2018-12-06.R
+++ 4_mpp/mpp_2018-12-06.R
@@ -3,3 +3,12 @@
 #
 # largely following https://kbroman.org/qtl2/assets/vignettes/user_guide.html
 # latest version of this script at https://bit.ly/rqtl2_2018
+
+# load R/qtl2
+library(qtl2)
+
+# load data set from yesterday
+library(qtl)
+sug <- read.cross("csv", "http://rqtl.org", "sug.csv",
+                  genotypes=c("CC", "CB", "BB"),
+                  alleles=c("C", "B"))
